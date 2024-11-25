6b8a4ff9b0e0d10be5e346725d6d07fe5cfa1f1c
divyani321 <divyanirana96@gmail.com>
2024-11-25 14:23:58 +0000

    first push

---
+++ product.js
@@ -0,0 +1,99 @@
+let products = [
+  {
+    "id": 1001,
+    "title": "Basketball Training",
+    "description": "Improve your basketball skills with professional coaching.",
+    "price": 50.00,
+    "image": "images/basketball.jpg",
+    "availableInventory": 10,
+    "rating": 4
+  },
+  {
+    "id": 1002,
+    "title": "Coding for Kids",
+    "description": "Learn the basics of coding and create your own apps and games.",
+    "price": 75.00,
+    "image": "images/codingforkids.jpg",
+    "availableInventory": 10,
+    "rating": 5
+  },
+  {
+    "id": 1003,
+    "title": "Art Class",
+    "description": "Discover your artistic side with painting, drawing, and sculpting.",
+    "price": 40.00,
+    "image": "images/artclass.jpg",
+    "availableInventory": 10,
+    "rating": 4
+  },
+  {
+    "id": 1004,
+    "title": "Dance Class",
+    "description": "Learn hip-hop, ballet, or contemporary dance with expert instructors.",
+    "price": 45.00,
+    "image": "images/danceclass.jpg",
+    "availableInventory": 10,
+    "rating": 5
+  },
+  
+  {
+    "id": 1006,
+    "title": "Music Lessons",
+    "description": "Learn to play an instrument with one-on-one lessons tailored to your skill level.",
+    "price": 30.00,
+    "image": "images/musiclessons.jpg",
+    "availableInventory": 10,
+    "rating": 4
+  },
+  
+  {
+    "id": 1008,
+    "title": "Chess Club",
+    "description": "Sharpen your mind and strategy skills in an after-school chess club.",
+    "price": 25.00,
+    "image": "images/chessclub.jpg",
+    "availableInventory": 10,
+    "rating": 4
+  },
+  {
+    "id": 1009,
+    "title": "Robotics Club",
+    "description": "Build robots and learn the basics of robotics with hands-on projects.",
+    "price": 80.00,
+    "image": "images/roboticsclub.jpg",
+    "availableInventory": 10,
+    "rating": 5
+  },
+  {
+    "id": 1010,
+    "title": "Photography Class",
+    "description": "Learn the art of photography, from basics to advanced techniques.",
+    "price": 55.00,
+    "image": "images/photographyclass.jpg",
+    "availableInventory": 10,
+    "rating": 4
+  },
+  {
+    "id": 1011,
+    "title": "Book Club: Mystery Novels",
+    "description": "Join our book club and dive into thrilling mystery novels with weekly discussions.",
+    "price": 15.00,
+    "image": "images/mysterynovels.jpg",
+    "availableInventory": 10,
+    "rating": 5
+  },
+  
+  
+  {
+    "id": 1014,
+    "title": "Poetry Club",
+    "description": "Write and share poems, and learn the art of expression through verse.",
+    "price": 20.00,
+    "image": "images/poetryclub.jpg",
+    "availableInventory": 10,
+    "rating": 4
+  },
+  
+  
+  
+];
